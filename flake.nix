d0ddb2d5fd7bb6998a6f44e3f3d506a09ea64b87
Petar Kirov <petar.p.kirov@gmail.com>
2024-11-27 16:20:05 +0200

    Initial Nix Flake containing 2 packages and a default dev shell

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-darwin" "aarch64-linux"];
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            docker-client
+            opentofu
+            kubectl
+            jq
+            nixos-rebuild
+          ];
+        };
+
+        packages.get-disk-usage = pkgs.writeShellScriptBin "get-disk-usage" ''
+          du -sh /etc
+        '';
+
+        packages.hello-python = pkgs.writers.writePython3Bin "hello-python" {} ''
+          print("Hello from Python")
+        '';
+      };
+    };
+}
